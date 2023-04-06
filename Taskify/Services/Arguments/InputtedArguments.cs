a99e14190c9eb091696707e08855297921c2f0b4
Nia <108984073+kawaii-Code@users.noreply.github.com>
2023-04-06 14:48:24 +0300

    fix: Wrong input prompt for uri

--- Taskify/Services/Arguments/InputtedArguments.cs
+++ Taskify/Services/Arguments/InputtedArguments.cs
@@ -4,21 +4,21 @@ public class InputtedArguments : IArguments
 {
     public string GetUri()
     {
-        Console.Write("Moodle username > ");
-        string username = Console.ReadLine();
-        return username;
+        Console.Write("Uri: ");
+        string uri = Console.ReadLine();
+        return uri;
     }
 
     public string? GetFilepath()
     {
-        Console.Write("File to write to > ");
+        Console.Write("File to write to: ");
         string filepath = Console.ReadLine();
         return filepath;
     }
 
     public bool IsPreview()
     {
-        Console.Write("Show preview? y/n > ");
+        Console.Write("Show preview? y/n: ");
         string response = Console.ReadLine();
         return response?.ToLower() == "y";
     }
