1bb37d2a395c5f1b99fea70ac08bd88b673da151
Nia <108984073+kawaii-Code@users.noreply.github.com>
2023-04-07 13:10:03 +0300

    feature: Task number incrementing

--- Taskify/Services/TaskDescriptionBuilder/CSharpLineBuilder.cs
+++ Taskify/Services/TaskDescriptionBuilder/CSharpLineBuilder.cs
@@ -4,10 +4,12 @@ namespace Taskify.Services.TaskDescriptionBuilder;
 
 public class CSharpLineDecorator : ITaskDescriptionDecorator
 {
+    private int _taskIndex = 1;
+    
     public string DecorateLine(string line)
     {
         StringBuilder csLineBuilder = new();
-        csLineBuilder.AppendLine("// == TASK 1 ==");
+        csLineBuilder.AppendLine($"// == TASK {_taskIndex++} ==");
         csLineBuilder.Append("// ");
         csLineBuilder.AppendLine(string.Join("\n// ", line.Split("\n")));
         return csLineBuilder.ToString();
